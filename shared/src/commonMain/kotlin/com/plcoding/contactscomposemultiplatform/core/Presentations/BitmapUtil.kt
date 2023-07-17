ad46b310b4cd59cfb309b0cb1cf256e8b87c69bf
Vishesh <vishesht27@gmail.com>
2023-07-18 00:00:00 +0530

    Added bitmap

---
+++ shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/core/Presentations/BitmapUtil.kt
@@ -0,0 +1,7 @@
+package com.plcoding.contactscomposemultiplatform.core.Presentations
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.ImageBitmap
+
+@Composable
+expect fun rememberBitmapFromBytes(bytes: ByteArray?): ImageBitmap?
